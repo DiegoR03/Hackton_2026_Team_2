6b039b6073d5a6f3b3d4e75e5a5bfa626ba983fe
DiegoR03 <Diegoramon@live.nl>
2026-03-25 15:07:48 +0100

    footer partial finished

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ fetch("partials/nav.html")
     .then(data => {
     document.getElementById("nav-placeholder").innerHTML = data;
     });
+
+fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(data => {
+    document.getElementById("footer-placeholder").innerHTML = data;
+    });
